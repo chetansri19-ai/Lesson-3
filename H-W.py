1ccacc198342d6fe9b3b080b142f0fccc9fc1fea
chetansri19-ai <chetansri19@gmail.com>
2025-10-30 17:21:11 +1100

    My H-W

---
+++ H-W.py
@@ -0,0 +1,7 @@
+name="Chetan"
+age=12
+achievement=True
+no_of_km=7.8
+
+
+print(f"Congratulations {name}! At the age of {age}, you have been able to run {no_of_km} kilometers at cross country!I can't believe that it is {achievement} you came first in the whole school!")
---
+++ extra.py
@@ -0,0 +1,10 @@
+name="Skibidi"
+print(name[3])
+print(name[-4])
+print(name[1:6])
+print(name[1:7])
+
+print(len(name)+3)
+print(name.lower())
+print(name.upper())
+print(name.capitalize())
